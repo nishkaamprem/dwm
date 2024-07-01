93e346b95677e1b9c82a46122c84a7765a5a0c7a
nishkaamprem <madhavmaharajji@gmail.com>
2024-07-01 18:19:16 +0545

    added bind to change keyboard layout

--- config.def.h
+++ config.def.h
@@ -65,6 +65,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+     { MODKEY|ControlMask,		  XK_space,  spawn,	         SHCMD("cycle-keyboard") },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_x,      killclient,     {0} },
